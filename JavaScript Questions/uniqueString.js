eca887c26574e8de7f11ff6041fb65c788d6ecad
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2022-06-05 02:59:30 -0700

    add q2

---
+++ JavaScript Questions/uniqueString.js
@@ -0,0 +1,13 @@
+const uniqueStr = (str) => {
+    const arr = []
+    for (let char of str) {
+        if (!arr.includes(char)) {
+            arr.push(char)
+        }
+    }
+    return arr.join('')
+}
+
+const uniqueStr2 = (str) => {
+    return [...new Set([...str])].join('')
+}
