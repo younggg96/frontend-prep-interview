0bc9e637ef62a200146d231d92610989d6d0a8b2
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2023-10-10 23:51:14 -0700

    new update lc

--- Leetcode Questions/Heap(Priority Queue)/lc658-FindKClosestElements.js
+++ Leetcode Questions/Heap(Priority Queue)/lc658-FindKClosestElements.js
@@ -1,31 +1,29 @@
-// Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+// 658. Find K Closest Elements
+// Medium
+// Topics
+// Companies
+// Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
 
-// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+// An integer a is closer to x than an integer b if:
 
-// You must find a solution with a memory complexity better than O(n2).
+// |a - x| < |b - x|, or
+// |a - x| == |b - x| and a < b
 
 // Example 1:
 
-// Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
-// Output: 13
-// Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+// Input: arr = [1,2,3,4,5], k = 4, x = 3
+// Output: [1,2,3,4]
 // Example 2:
 
-// Input: matrix = [[-5]], k = 1
-// Output: -5
+// Input: arr = [1,2,3,4,5], k = 4, x = -1
+// Output: [1,2,3,4]
 
 // Constraints:
 
-// n == matrix.length == matrix[i].length
-// 1 <= n <= 300
-// -109 <= matrix[i][j] <= 109
-// All the rows and columns of matrix are guaranteed to be sorted in non-decreasing order.
-// 1 <= k <= n2
-
-// Follow up:
-
-// Could you solve the problem with a constant memory (i.e., O(1) memory complexity)?
-// Could you solve the problem in O(n) time complexity? The solution may be too advanced for an interview but you may find reading this paper fun.
+// 1 <= k <= arr.length
+// 1 <= arr.length <= 104
+// arr is sorted in ascending order.
+// -104 <= arr[i], x <= 104
 
 class MyPriorityQueue {
   // 创建优先队列的构造函数，参数是一个比较函数 comparator
@@ -112,25 +110,26 @@ class MyPriorityQueue {
 }
 
 /**
- * @param {number[][]} matrix
+ * @param {number[]} arr
  * @param {number} k
- * @return {number}
+ * @param {number} x
+ * @return {number[]}
  */
-var kthSmallest = function (matrix, k) {
-  const pq = new MyPriorityQueue((a, b) => a[0] - b[0]);
-  for (let i = 0; i < matrix.length; i++) {
-    pq.enqueue([item[0], i, 0]);
-  }
-  let res = -1;
-  while (k > 0 && !pq.isEmpty()) {
-    const cur = pq.pop();
-    k--;
-    res = cur[0];
-    const i = cur[1];
-    const j = cur[2];
-    if (j + 1 < matrix[0].length) {
-      pq.enqueue([matrix[i][j + 1], i, j + 1]);
+var findClosestElements = function (arr, k, x) {
+  const pq = new MyPriorityQueue((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
     }
+    return a[1] - b[1];
+  });
+  for (let i = 0; i < arr.length; i++) {
+    pq.enqueue([arr[i], Math.abs(arr[i] - x)]);
   }
+  let res = [];
+  for (let i = 0; i < k; i++) {
+    const ele = pq.dequeue();
+    res.push(ele[0]);
+  }
+  res.sort((a, b) => a - b);
   return res;
 };
