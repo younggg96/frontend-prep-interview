6262c44d942b6979326dbc3ed6b5aeb80f5f8927
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2023-11-02 01:45:04 -0700

    new updates

---
+++ Leetcode Questions/LinkedList/lc445-AddTwoNumbersII.js
@@ -0,0 +1,36 @@
+// 445. Add Two Numbers II
+// Medium
+// Topics
+// Companies
+// You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Example 1:
+
+// Input: l1 = [7,2,4,3], l2 = [5,6,4]
+// Output: [7,8,0,7]
+// Example 2:
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [8,0,7]
+// Example 3:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    
+};
