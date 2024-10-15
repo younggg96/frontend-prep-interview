f7bf331f82fcb7368ae77320f08d9d4b9be27fc8
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2024-10-15 10:24:26 -0700

    update

---
+++ JavaScript Questions/ImplementMathSqrt.js
@@ -0,0 +1,35 @@
+// Math.sqrt() helps us getting the square root of a number.
+
+// Can your write your own mySqrt() ? You should return the integer part only, truncating the fraction part.
+
+// mySqrt(0)
+// // 1
+// mySqrt(1)
+// // 1
+// mySqrt(2)
+// // 1
+// mySqrt(4)
+// // 2
+// mySqrt(NaN)
+// // NaN
+// Attention for the special case listed up in the spec.
+
+// Follow-up
+
+// What is time & space complexity of your solution ? Can you do better?
+
+
+/**
+ * @param {any} x
+ * @return {number}
+ */
+function mySqrt(x) {
+    // your code here
+    // if(x < 0 || isNaN(x) || typeof x !== 'number') return NaN;
+
+    // let start = 0;
+    // let end = Math.floor(x/2);
+    // let ans = 1;
+
+    // while()
+  }
